109a524183945bad6b17a24fc8c4b71a4f024cdf
cassiwilliams <cassiwelter@hotmail.com>
2020-06-02 15:28:48 -0400

    Create repo for Chapter 2 Studio

---
+++ Studio2.6/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studio2._6
+{
+    class Circle
+    {        
+        public static double CalculateArea(double radius)
+        {            
+            return Math.Pow(radius, 2) * Math.PI;
+        }
+        public static double CalculateCircum(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        public static double CalculateDiameter(double radius)
+        {
+            return 2 * radius;
+        }
+
+    }
+}
---
+++ Studio2.6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Studio2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;                       
+
+            double milesPerGallon;         
+            double numGallons;
+
+            string input;
+
+            Console.WriteLine("Enter the radius of a circle in a number with no more than two decimal points.");
+            input = Console.ReadLine();
+            radius = double.Parse(input);       
+                     
+            Console.WriteLine("Information about the circle: ");
+            Console.WriteLine("Area: " + Math.Round(Circle.CalculateArea(radius), 2));
+            Console.WriteLine("Circumference: " + Math.Round(Circle.CalculateCircum(radius)));
+            Console.WriteLine("Diameter: " + Math.Round(Circle.CalculateDiameter(radius)));         
+
+            Console.WriteLine("Enter the miles per gallon that your vehicle gets.");
+            input = Console.ReadLine();
+            milesPerGallon = double.Parse(input);
+
+            numGallons = Circle.CalculateCircum(radius) / milesPerGallon;
+            Console.WriteLine("You will need " + numGallons + "to drive the distance of the circle's circumference.");
+        }
+    }
+}
